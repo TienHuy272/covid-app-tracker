b9b1aa706187f25e3b8d2ff639dbc4b4c1dd43e4
huynt <tienhuy1995@gmail.com>
2020-05-23 19:29:09 +0700

    Updates

---
+++ precache-manifest.14e2bd57e8ed330cd2afb7ba306a5c46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb1895b3c1f6cb618171b07650876fed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6b16f41274126265163",
+    "url": "/static/css/main.f98c1463.chunk.css"
+  },
+  {
+    "revision": "98d32b9d4256de358e76",
+    "url": "/static/js/2.c3f72cf8.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.c3f72cf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6b16f41274126265163",
+    "url": "/static/js/main.1722b4d5.chunk.js"
+  },
+  {
+    "revision": "da4363da10beba560b62",
+    "url": "/static/js/runtime-main.a24ea9e0.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
